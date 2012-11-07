1ca1aea947fe8bd876a55cd959fdd51a9dcabc61
Alexandra Magin <alexandra@magin.org>
2012-11-07 09:41:41 -0800

    more interesting color pattern don't construct a image_data every time add the load event listener the correct way

--- munch-canvas.js
+++ munch-canvas.js
@@ -8,23 +8,30 @@ function munch() {
   var ctx = canvas.getContext('2d');
   var t = 0;
   var y = 0
-  
+
+  var image_data = ctx.createImageData(dim, dim);
+  // setup the alpha channel
+  for (i = 3; i <= dim * dim * 4; i += 4) {
+    image_data.data[i] = 255;
+  }
+
   function do_one_frame() {
-    var image_data = ctx.createImageData(dim, dim);
     for (y = 0; y < dim; y++) {
       for (x = 0; x < dim; x++) {
-        var v = ((x ^ y) < t) ? 255 : 0;
-        image_data.data[y*dim*4 + x*4 + 0] = v;
-        image_data.data[y*dim*4 + x*4 + 1] = v;
-        image_data.data[y*dim*4 + x*4 + 2] = v;
-        image_data.data[y*dim*4 + x*4 + 3] = 255;
+        var offset = y*dim*4 + x*4;
+        image_data.data[offset] = ((t - dim/2) - (x ^ y));
+        image_data.data[offset + 1] = (t - (x ^ y));
+        image_data.data[offset + 2] = ((t - dim) - (x ^ y));
       }
     }
     ctx.putImageData(image_data, 0, 0);
-    t++;
+    t += 5;
+    if (t > dim) {
+      t = 0;
+    }
   }
 
-  var interval_id = window.setInterval(do_one_frame, 50);
+  var interval_id = window.setInterval(do_one_frame, 20);
 }
 
-window.onload = munch;
+window.addEventListener('load', munch);
