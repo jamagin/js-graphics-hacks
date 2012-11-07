2ea897f7fab84a8c4fe46a074f445c0fd6683380
Alexandra Magin <alexandra@magin.org>
2012-11-07 11:58:43 -0800

    no need to be square

--- munch-canvas.js
+++ munch-canvas.js
@@ -1,32 +1,29 @@
 function munch() {
   var canvas = document.getElementById('maincanvas');
-  var dim = window.innerWidth < window.innerHeight ? 
-    window.innerWidth :
-    window.innerHeight;
-  canvas.width = dim;
-  canvas.height = dim;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   var ctx = canvas.getContext('2d');
   var t = 0;
   var y = 0
 
-  var image_data = ctx.createImageData(dim, dim);
+  var image_data = ctx.createImageData(canvas.width, canvas.height);
   // setup the alpha channel
-  for (i = 3; i <= dim * dim * 4; i += 4) {
+  for (i = 3; i <= canvas.width * canvas.height * 4; i += 4) {
     image_data.data[i] = 255;
   }
 
   function do_one_frame() {
-    for (y = 0; y < dim; y++) {
-      for (x = 0; x < dim; x++) {
-        var offset = y*dim*4 + x*4;
-        image_data.data[offset] = ((t - dim/2) - (x ^ y));
+    for (y = 0; y < canvas.height; y++) {
+      for (x = 0; x < canvas.width; x++) {
+        var offset = y * canvas.width * 4 + x * 4;
+        image_data.data[offset] = ((t - canvas.height) - (x ^ y));
         image_data.data[offset + 1] = (t - (x ^ y));
-        image_data.data[offset + 2] = ((t - dim) - (x ^ y));
+        image_data.data[offset + 2] = ((t - canvas.height * 2) - (x ^ y));
       }
     }
     ctx.putImageData(image_data, 0, 0);
     t += 5;
-    if (t > dim * 2) {
+    if (t > canvas.height * 2) {
       t = 0;
     }
   }
