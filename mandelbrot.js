6400f9d79d9db060d625308bf5c659441be1f541
Alexandra Magin <alexandra@magin.org>
2012-11-09 16:29:25 -0800

    blah

--- mandelbrot.js
+++ mandelbrot.js
@@ -3,8 +3,7 @@ function munch() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   var ctx = canvas.getContext('2d');
-  var t = 0;
-  var y = 0
+  var t = 100;
 
   var image_data = ctx.createImageData(canvas.width, canvas.height);
   // setup the alpha channel
@@ -12,13 +11,15 @@ function munch() {
     image_data.data[i] = 255;
   }
 
+    
   function do_one_frame() {
     for (y = 0; y < canvas.height; y++) {
       for (x = 0; x < canvas.width; x++) {
         var offset = y * canvas.width * 4 + x * 4;
-        image_data.data[offset] = (t - (x ^ y)) % 256;
-        image_data.data[offset + 1] = ((t - 128) - (x ^ y)) % 256;
-        image_data.data[offset + 2] = ((t - 64) - (x ^ y)) % 256;
+
+          image_data.data[offset] = x % 255;
+          image_data.data[offset + 1] = y % 255;
+
       }
     }
     ctx.putImageData(image_data, 0, 0);
@@ -28,7 +29,7 @@ function munch() {
     }
   }
 
-  var interval_id = window.setInterval(do_one_frame, 10);
+  do_one_frame();
 }
 
 window.addEventListener('load', munch);
