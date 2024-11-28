b6e06dd0316594b87f56d11ff041f58de0f9f870
Alexandra Magin <alexandra@magin.org>
2024-11-28 13:35:03 -0800

    made a random walk and a hilbert curve

--- random-walk.js
+++ random-walk.js
@@ -7,40 +7,13 @@ canvas.height = window.innerHeight;
 var ctx = canvas.getContext('2d');
 
 var image_data = ctx.createImageData(canvas.width, canvas.height);
+var pixel_to_unit_interval = pixel_range_to_unit_interval(canvas.width * canvas.height);
 
-function l_system() {
-  // this one is for a hilbert curve
-  var state = "A"; // the axiom
-  var rules = {
-    "A": "+BF-AFA-FB+",
-    "B": "-AF+BFB+FA-",
-  };
-  return function () {
-    var output = "";
-    for (let i = 0; i < state.length; i++) {
-      let production = rules[state[i]];
-      if (typeof production !== 'undefined') {
-        output += production;
-      } else {
-        output += state[i];
-      }
-    }
-    state = output;
-    return output;
-  }
-}
-
+// this is only going to look good for 
 function do_one_frame() {
-  var produce = l_system();
-  var moves;
-  for (let i = 0; i < 9; i++) {
-    moves = produce();
-  }
-  var moves = produce();
-  var pixel_to_unit_interval = pixel_range_to_unit_interval(moves.match(/F/g).length);
-  var x = 0;
-  var y = 0;
-  var dir = 1;
+  var x = Math.floor(canvas.width / 2);
+  var y = Math.floor(canvas.height / 2);
+  var dir = 0;
   var accum = 0;
   function plot() {
     accum++;
@@ -50,7 +23,14 @@ function do_one_frame() {
     image_data.data[offset + 2]] = hsv_to_rgb(pixel_to_unit_interval(accum), 1, 1);
     image_data.data[offset + 3] = 255; // alpha channel
   }
-
+  function oob(x, y) {
+    if (x < 0 || y < 0) {
+      return true;
+    } else if (x >= canvas.width || y >= canvas.height) {
+      return true;
+    }
+    return false;
+  }
   function next_point() {
     var dirs = [
       [1, 0],
@@ -65,16 +45,30 @@ function do_one_frame() {
   }
   function fwd() {
     var [new_x, new_y] = next_point();
-    x = new_x;
-    y = new_y;
-    plot();
+    if (oob(new_x, new_y)) {
+      return false;
+    } else {
+      x = new_x;
+      y = new_y;
+      plot();
+      return true;
+    }
   }
-
   function left() {
     var old_dir = dir;
     dir--;
     if (dir < 0) {
       dir = 3;
+    }
+    var [new_x, new_y] = next_point();
+    if (oob(new_x, new_y)) {
+      dir = old_dir;
+      return false;
+    } else {
+      x = new_x;
+      y = new_y;
+      plot();
+      return true;
     }
   }
   function right() {
@@ -83,18 +77,33 @@ function do_one_frame() {
     if (dir > 3) {
       dir = 0;
     }
+    var [new_x, new_y] = next_point();
+    if (oob(new_x, new_y)) {
+      dir = old_dir;
+      return false;
+    } else {
+      x = new_x;
+      y = new_y;
+      plot();
+      return true;
+    }
   }
-  for (let i = 0; i < moves.length; i++) {
-    switch (moves[i]) {
-      case "F":
-        fwd();
+  while (accum < canvas.width * canvas.height) {
+    var choice = Math.floor(Math.random() * 3);
+    var result;
+    switch (choice) {
+      case 0:
+        result = fwd();
         break;
-      case "+":
-        left();
+      case 1:
+        result = left();
         break;
-      case "-":
-        right();
+      case 2:
+        result = right();
         break;
+    }
+    if (result) {
+      accum++;
     }
   }
   ctx.putImageData(image_data, 0, 0);
