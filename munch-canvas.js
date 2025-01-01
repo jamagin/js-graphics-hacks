3b83cc1357bd415e6cb426304f8016896a7cdc55
Alexandra Magin <alexandra@magin.org>
2025-01-01 09:28:18 -0800

    wip

--- munch-canvas.js
+++ munch-canvas.js
@@ -1,34 +1,50 @@
-function munch() {
-  var canvas = document.getElementById('maincanvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  var ctx = canvas.getContext('2d');
-  var t = 0;
-  var y = 0
+import { pixel_range_to_unit_interval, hsv_to_rgb } from "./utils.js";
 
-  var image_data = ctx.createImageData(canvas.width, canvas.height);
-  // setup the alpha channel
-  for (i = 3; i <= canvas.width * canvas.height * 4; i += 4) {
-    image_data.data[i] = 255;
-  }
+var canvas = document.getElementById('maincanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext('2d');
+var t = 0;
+var y = 0
 
-  function do_one_frame() {
-    for (y = 0; y < canvas.height; y++) {
-      for (x = 0; x < canvas.width; x++) {
-        var offset = y * canvas.width * 4 + x * 4;
-        image_data.data[offset] = (t - (x ^ y)) % 256;
-        image_data.data[offset + 1] = ((t - 128) - (x ^ y)) % 256;
-        image_data.data[offset + 2] = ((t - 64) - (x ^ y)) % 256;
-      }
-    }
-    ctx.putImageData(image_data, 0, 0);
-    t += 5;
-    if (t > canvas.height * 2) {
-      t = 0;
-    }
-  }
-
-  var interval_id = window.setInterval(do_one_frame, 10);
+var pixels_total = canvas.width * canvas.height;
+var pixel_to_unit_interval = pixel_range_to_unit_interval(256);
+var image_data = ctx.createImageData(canvas.width, canvas.height);
+// setup the alpha channel
+for (var i = 3; i <= canvas.width * canvas.height * 4; i += 4) {
+  image_data.data[i] = 255;
 }
 
-window.addEventListener('load', munch);
+var pixelGen = function (x, y, t) {
+  return [
+    (t - (x ^ y)) % 256,
+    ((t - 128) - (x ^ y)) % 256,
+    ((t - 64) - (x ^ y)) % 256,
+  ];
+}
+var [_, mode] = window.location.href.split('#');
+if (mode === 'hsv') {
+  pixelGen = function (x, y, t) {
+    return hsv_to_rgb(pixel_to_unit_interval((t - (x ^ y)) % 256), 0.5, 0.5)
+  }
+}
+
+function do_one_frame() {
+  for (var y = 0; y < canvas.height; y++) {
+    for (var x = 0; x < canvas.width; x++) {
+      var offset = y * canvas.width * 4 + x * 4;
+      [image_data.data[offset],
+      image_data.data[offset + 1],
+      image_data.data[offset + 2]] = pixelGen(x, y, t);
+    }
+  }
+  ctx.putImageData(image_data, 0, 0);
+  t += 5;
+  if (t > canvas.height * 2) {
+    t = 0;
+  }
+}
+
+var interval_id = window.setInterval(do_one_frame, 10);
+
+
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 const name = 'utils';
 
+// import { parseWat } from "./wabt.js";
+
 function pixel_range_to_unit_interval(n) {
     return function (x) {
         return x / n;
@@ -17,5 +19,20 @@ function hsv_to_rgb(h, s, v) {
     }
     return [f(5), f(3), f(1)];
 }
+
+
+// maximal sequence for a 16 bit LFSR, per wikipedia
+// period = 65535 = 3 * 5 * 17 * 257
+// mask = binary 1101 0000 0000 1000
+// function lfsr(mask = 0xd008, init_state = 0x45) {
+//     const wasmModule = wabt.parseWat("lfsr.wat", fetch("lfsr.wat"));
+//     const { buffer } = wasmModule.toBinary({ write_debug_names: true });
+//     const module = aWait WebAssembly.compile(buffer);
+//     let state_g = new WebAssembly.Global({ value: "i32", mutable: true }, init_state);
+//     let mask_g = new WebAssembly.Global({ value: "i32", mutable: false }, mask);
+//     let instance = aWait WebAssembly.instantiate(module, { env: { state: state_g, mask: mask_g } });
+//     return instance.exports.lfsrInner;
+// }
+
 
 export { name, pixel_range_to_unit_interval, hsv_to_rgb };
