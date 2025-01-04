b04332ae5ebf9df013fc81cbc8310711a22650ad
Alexandra Magin <alexandra@magin.org>
2025-01-04 10:00:09 -0800

    factor out graphics library, use in munch-canvas, modernize

--- munch-canvas.js
+++ munch-canvas.js
@@ -1,34 +1,27 @@
-function munch() {
-  var canvas = document.getElementById('maincanvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  var ctx = canvas.getContext('2d');
-  var t = 0;
-  var y = 0
+import { Graphics2D } from "./utils.js";
 
-  var image_data = ctx.createImageData(canvas.width, canvas.height);
-  // setup the alpha channel
-  for (i = 3; i <= canvas.width * canvas.height * 4; i += 4) {
-    image_data.data[i] = 255;
-  }
+var canvas = document.getElementById('maincanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var g = new Graphics2D(canvas);
+var t = 0;
 
-  function do_one_frame() {
-    for (y = 0; y < canvas.height; y++) {
-      for (x = 0; x < canvas.width; x++) {
-        var offset = y * canvas.width * 4 + x * 4;
-        image_data.data[offset] = (t - (x ^ y)) % 256;
-        image_data.data[offset + 1] = ((t - 128) - (x ^ y)) % 256;
-        image_data.data[offset + 2] = ((t - 64) - (x ^ y)) % 256;
-      }
-    }
-    ctx.putImageData(image_data, 0, 0);
-    t += 5;
-    if (t > canvas.height * 2) {
-      t = 0;
+function do_one_frame() {
+  for (var y = 0; y < g.height; y++) {
+    for (var x = 0; x < g.width; x++) {
+      g.put(x, y, [
+        (t - (x ^ y)) % 256,
+        ((t - 128) - (x ^ y)) % 256,
+        ((t - 64) - (x ^ y)) % 256,
+      ])
     }
   }
-
-  var interval_id = window.setInterval(do_one_frame, 10);
+  g.draw();
+  t += 5;
+  if (t > g.height * 2) {
+    t = 0;
+  }
 }
 
-window.addEventListener('load', munch);
+var interval_id = window.setInterval(do_one_frame, 10);
+
--- utils.js
+++ utils.js
@@ -18,4 +18,72 @@ function hsv_to_rgb(h, s, v) {
     return [f(5), f(3), f(1)];
 }
 
-export { name, pixel_range_to_unit_interval, hsv_to_rgb };
+
+class Graphics2D {
+    constructor(canvas) {
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.ctx = canvas.getContext('2d');
+        this.data = this.ctx.createImageData(this.width, this.height); // blank black
+    }
+
+    put(x, y, rgb) {
+        var offset = (y * this.width + x) * 4;
+        [this.data.data[offset],
+        this.data.data[offset + 1],
+        this.data.data[offset + 2]] = rgb;
+        this.data.data[offset + 3] = 255; // alpha channel
+    }
+
+    get(x, y) {
+        var offset = (y * this.width + x) * 4;
+        return this.data.data.slice(offset, offset + 4);
+    }
+
+    doLine(x1, y1, x2, y2, action, combiner, arg) {
+        var sum = [];
+        if (x1 === x2) {
+            var [start, end] = [y1, y2].toSorted();
+            for (var y = start; y <= end; y++) {
+                sum = combiner(sum, action(x1, y, arg));
+            }
+        } else if (y1 === y2) {
+            var [start, end] = [x1, x2].toSorted();
+            for (var x = start; x <= end; x++) {
+                sum = combiner(sum, action(x, y1, arg));
+            }
+        }
+        return sum
+    }
+
+    noOp(sum, another) { }
+
+    plotLine(x1, y1, x2, y2, rgb) {
+        var put = this.put.bind(this);
+        return this.doLine(x1, y1, x2, y2, put, this.noOp, rgb);
+    }
+
+    collectOp(sum, another) {
+        return sum.concat(another);
+    }
+
+    checkLine(x1, y1, x2, y2) {
+        var get = this.get.bind(this);
+        var pixels = this.doLine(x1, y1, x2, y2, get, this.collectOp);
+        return pixels.every(a => a[3] === 0); // alpha channel
+    }
+
+    oob(x, y) {
+        if (x < 0 || y < 0) {
+            return true;
+        } else if (x >= this.width || y >= this.height) {
+            return true;
+        }
+        return false;
+    }
+
+    draw() {
+        this.ctx.putImageData(this.data, 0, 0);
+    }
+}
+export { name, pixel_range_to_unit_interval, hsv_to_rgb, Graphics2D };
